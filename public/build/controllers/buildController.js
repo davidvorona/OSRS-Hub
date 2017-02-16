9d58374517b131c6de320a1fdc29f659d1b0882e
davidvorona <davidavorona@gmail.com>
2017-02-16 12:49:29 -0800

    spread/pure calc finished

--- public/build/controllers/buildController.js
+++ public/build/controllers/buildController.js
@@ -1,55 +1,128 @@
-const findCombatObj = (combatLvl, buildType) => {
-    const combatObj = {
-        attack: 1,
-        defence: 1,
-        strength: 1,
-        hitpoints: 1,
-        ranged: 1,
-        magic: 1,
-        prayer: 1
-    };
-    if (buildType === "Spread") {
-        const spreadLvl = Math.round((combatLvl * 4) / 5.1);
-        return ({
-            attack: spreadLvl,
-            defence: spreadLvl,
-            strength: spreadLvl,
-            hitpoints: spreadLvl,
-            ranged: spreadLvl,
-            magic: spreadLvl,
-            prayer: spreadLvl
-        });
-    } else {
-        const pureType = buildType.toLowerCase();
-        let adaptedCmb;
-        let pureLvl;
-        if (pureType === "magic" || "ranged") {
-            adaptedCmb = combatLvl - 2.5;
-            pureLvl = Math.ceil((adaptedCmb * 4) / 1.3);
-        } else if (pureType === "defence") {
-            adaptedCmb = combatLvl - 2.8;
-            pureLvl = Math.ceil(adaptedCmb * 4);
-        } else if (pureType === "attack" || "strength") {
-            adaptedCmb = combatLvl - 3.8;
-            pureLvl = Math.ceil((adaptedCmb * 4) / 1.3);
-        }
-        combatObj[pureType] = pureLvl <= 99 ? pureLvl : 99;
-        return combatObj;
-    }
+// TODO: keep copy of main data object out of
+// controller scope for access between SPA Angular routes;
+// only transformed upon posting to db,
+// then reset to default vals
+let combatToPG = [
+    { skill: "Attack", level: 1 },
+    { skill: "Defence", level: 1 },
+    { skill: "Strength", level: 1 },
+    { skill: "Hitpoints", level: 10 },
+    { skill: "Ranged", level: 1 },
+    { skill: "Magic", level: 1 },
+    { skill: "Prayer", level: 1 }
+];
+
+const toObject = (data) => {
+    const obj = {};
+    data.forEach((el) => {
+        obj[el.skill] = el.level;
+    });
+    return obj;
 };
 
+const toDataStructure = (data) => {
+    const arrOfObj = [];
+    Object.keys(data).forEach((key, i) => {
+        arrOfObj[i] = {};
+        arrOfObj[i].skill = key;
+        arrOfObj[i].level = data[key];
+    });
+    return arrOfObj;
+};
+
+// still in dev:
+// use remainingCmb to get how many
+// possible lvls left in each skill
+const extractRemainingCmb = (pureLvl, pureType) => {
+    if (pureLvl <= 99) return;
+    let adaptedCmb;
+    let maxCmb;
+    let remainingCmb = {};
+    if (pureType === "Magic" || pureType === "Ranged") {
+        maxCmb = 99 * 1.5;
+        maxCmb = (maxCmb * 1.3) + 11;
+        adaptedCmb = pureLvl * 1.5;
+        adaptedCmb = (adaptedCmb * 1.3) + 11;
+        remainingCmb = adaptedCmb - maxCmb;
+    } else if (pureType === "Defence") {
+        maxCmb = 99 + 12;
+        adaptedCmb = pureLvl + 12;
+        remainingCmb = adaptedCmb - maxCmb;
+    } else if (pureType === "Attack" || pureType === "Strength") {
+        maxCmb = (99 * 1.3) + 12.3;
+        adaptedCmb = (pureLvl * 1.3) + 12.3;
+        remainingCmb = adaptedCmb - maxCmb;
+    }
+    console.log("remainingCmb", remainingCmb);
+};
+
+const calculatePure = (pureType, adaptedCmb) => {
+    let pureLvl;
+    if (pureType === "Magic" || pureType === "Ranged") {
+        pureLvl = Math.ceil((adaptedCmb - 11) / 1.3);
+        pureLvl = Math.ceil(pureLvl / 1.5);
+        extractRemainingCmb(pureLvl, pureType);
+    } else if (pureType === "Defence") {
+        pureLvl = Math.ceil(adaptedCmb - 12);
+        extractRemainingCmb(pureLvl, pureType);
+    } else if (pureType === "Attack" || pureType === "Strength") {
+        pureLvl = Math.ceil(Math.ceil(adaptedCmb - 12.3) / 1.3);
+        extractRemainingCmb(pureLvl, pureType);
+    }
+    return pureLvl;
+};
+
+const buildifyTable = (buildType, combatLvl, combatArr) => {
+    const combatObj = toObject(combatArr);
+
+    if (buildType === "Spread" && combatLvl > 12) {
+        let spreadLvl = Math.ceil(Math.ceil(combatLvl * 4) / 5.1);
+        spreadLvl = spreadLvl <= 99 ? spreadLvl : 99;
+        Object.keys(combatObj).forEach((keys) => {
+            combatObj[keys] = spreadLvl;
+        });
+        return toDataStructure(combatObj);
+    }
+
+    const pureType = buildType;
+    const adaptedCmb = Math.ceil(combatLvl * 4);
+    const pureLvl = calculatePure(pureType, adaptedCmb);
+
+    combatObj[pureType] = pureLvl <= 99 ? pureLvl : 99;
+
+    return toDataStructure(combatObj);
+};
+
+const buyingGf = (gp) => {
+    console.log(`Buying gf ${gp}gp`);
+};
+
+// src: http://www.runehq.com/calculator/combat-level
+const osCombatLevel = (combatObj) => {
+    const melee = combatObj.attack + combatObj.strength;
+    const range = Math.floor(1.5 * combatObj.ranged);
+    const mage = Math.floor(1.5 * combatObj.magic);
+    const high = Math.max(melee, range, mage);
+
+    const cmb = Math.floor(1.3 * high) + combatObj.defence +
+      combatObj.hitpoints + Math.floor(combatObj.prayer / 2);
+    return Math.floor(cmb / 4);
+};
+
+// tint row red on being selected as pureType
 angular.module("BuildController", ["ngRoute"])
-  .controller("BuildController", function BuildController() {
+  .controller("BuildController", function BuildController($scope) {
       const bc = this;
       bc.showBuild = false;
       bc.buildType = "Spread";
+      bc.combatArr = Object.assign(combatToPG); // shallow clone of object
 
       bc.submit = (combatLvl) => {
+          bc.combatLvl = combatLvl;
+          bc.combatArr = Object.assign(combatToPG);
+          bc.combatArr = buildifyTable(bc.buildType, combatLvl, bc.combatArr);
+          bc.displayCollection = buildifyTable(bc.buildType, combatLvl, bc.combatArr);
           bc.showBuild = true;
-          console.log(findCombatObj(combatLvl, bc.buildType));
-          // TODO: test if this actually works, then
-          // write algorithm to determine remaining points and
-          // update table in real-time based on points added
       };
 
       bc.displayTypes = () => {
@@ -58,5 +131,12 @@ angular.module("BuildController", ["ngRoute"])
 
       bc.choice = (buildType) => {
           bc.buildType = buildType;
+          bc.combatArr = Object.assign(combatToPG);
+          bc.combatArr = buildifyTable(bc.buildType, bc.combatLvl, bc.combatArr);
+          bc.displayCollection = buildifyTable(bc.buildType, bc.combatLvl, bc.combatArr);
       };
+
+      $scope.$on("$destroy", () => {
+          buyingGf(5);
+      });
   });
